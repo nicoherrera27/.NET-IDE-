16ac9cb4340db1479392dbec32009d992f6666dd
Nicolas Herrera <herreranico2703@gmail.com>
2024-04-15 20:58:39 -0300

    Add files via upload

---
+++ Unidad01/Capitulo1/Lab01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01{
+    internal class Program{
+        static void Main(string[] args){
+            System.Console.Write("Hola Mundo!");
+            System.Console.ReadKey();
+        }
+    }
+}
