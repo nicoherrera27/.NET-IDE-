25b72618df2cd3361e8e85e2cab3281e3b870510
Nicolas Herrera <herreranico2703@gmail.com>
2024-04-15 21:11:44 -0300

    Add files via upload

---
+++ Unidad02/Capitulo01/Lab01.ParaPensar/Lab01.ParaPensar/Program.cs
@@ -0,0 +1,13 @@
+﻿int n1, n2, suma;
+
+Console.WriteLine("Ingrese el primer numero: ");
+n1 = Console.Read();
+
+Console.WriteLine("Ingrese el segundo numero: ");
+n2 = Console.Read();
+
+suma = n1 + n2;
+Console.WriteLine("");
+
+Console.WriteLine("El resultado de la suma de ",n1," y ",n2,"es ",suma);
+
